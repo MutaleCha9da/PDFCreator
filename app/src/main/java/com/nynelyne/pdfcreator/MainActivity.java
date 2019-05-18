997ff8f683bae50da43ae4c600e5958f2c1b5da3
Mutale Chanda <mutalefbc@gmail.com>
2019-05-18 22:41:32 +0200

    Full program, tested. can't find the directory though. will fix that soon

--- app/src/main/java/com/nynelyne/pdfcreator/MainActivity.java
+++ app/src/main/java/com/nynelyne/pdfcreator/MainActivity.java
@@ -2,13 +2,89 @@ package com.nynelyne.pdfcreator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCreatePDF;
+    EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.hello);
+        btnCreatePDF = findViewById(R.id.createPDF);
+        editText = findViewById(R.id.editText);
+
+        btnCreatePDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createPdf(editText.getText().toString());
+            }
+        });
+    }
+
+    private void createPdf(String sometext)
+    {
+        PdfDocument document = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(300, 600, 1).create();
+
+        PdfDocument.Page page = document.startPage(pageInfo);
+        Canvas canvas = page.getCanvas();
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        canvas.drawCircle(50, 50, 30, paint);
+        paint.setColor(Color.BLACK);
+        canvas.drawText(sometext, 80, 50, paint);
+        document.finishPage(page);
+
+        //page 2
+        pageInfo = new PdfDocument.PageInfo.Builder(300, 600, 2).create();
+        page = document.startPage(pageInfo);
+        canvas = page.getCanvas();
+        paint = new Paint();
+        paint.setColor(Color.BLUE);
+        canvas.drawCircle(100, 100, 100, paint);
+        document.finishPage(page);
+
+        //Write the document content
+        String directory_path = Environment.getExternalStorageDirectory().getPath() + "/mypdf/";
+        String pathname;
+        File file = new File(directory_path);
+        if (!file.exists())
+        {
+            file.mkdirs();
+        }
+        String targetPdf = directory_path+"test-2.pdf";
+        File filePath = new File(targetPdf);
+        try {
+            document.writeTo(new FileOutputStream(filePath));
+            Toast.makeText(this, "Done", Toast.LENGTH_LONG).show();
+        } catch (IOException e)
+        {
+            Log.e("main", "error "+e.toString());
+            Toast.makeText(this, "Something wrong: " +e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+        //close the document
+        document.close();
     }
 }
